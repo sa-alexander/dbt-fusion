a16b0a10d0858616905220c9823deee05e492020
Sankey Alexander <sankey.alexander@dbtlabs.com>
2026-01-28 21:55:21 +0000

    new product code

--- models/marts/products.sql
+++ models/marts/products.sql
@@ -3,7 +3,21 @@ with
 products as (
 
     select * from {{ ref('stg_products') }}
+    select 
+        product_id,
+        product_name,
+        product_category,
+        price,
+        created_at
+    from {{ ref('stg_products') }}
 
 )
 
 select * from products
+select 
+    product_id,
+    product_name,
+    product_category,
+    price,
+    created_at
+from products
